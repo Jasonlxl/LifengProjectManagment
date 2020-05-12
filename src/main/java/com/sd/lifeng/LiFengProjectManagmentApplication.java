392001e4fb9acabdc23b20a234a660eb8718840f
zhangys <zhangyishen@jasonlxl.top>
2020-05-12 16:34:56 +0800

    创建砺峰项目

---
+++ src/main/java/com/sd/lifeng/LiFengProjectManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.sd.lifeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiFengProjectManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiFengProjectManagmentApplication.class, args);
+	}
+
+}
